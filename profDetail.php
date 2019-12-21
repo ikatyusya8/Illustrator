1a9d5b9a06825c1811991bea5bdb76649140cd5c
ikatyusya8 <48384384+ikatyusya8@users.noreply.github.com>
2019-12-21 16:11:04 +0900

    Create profDetail.php

---
+++ profDetail.php
@@ -0,0 +1,294 @@
+<?php 
+$siteTitle = 'イラストレーター　ユーザーページ';
+require('head.php');
+?>
+
+<body>
+  <?php
+  require('header.php');
+  ?>
+
+  <div class="profile">
+    <div class="prof-head"></div>
+    <div class="prof-icon"></div>
+    <div class="container">
+      <div class="user-name">user name</div>
+      <div class="follow">フォロー: 55</div>
+      <div class="follower">フォロワー: 70</div>
+    </div>
+  </div>
+  <section class="range-menu">
+    <form action="" method="post" class="open">
+      <div class="radio-tile-group">
+        <label class="input-container">
+          <input class="radio-button" type="radio" name="open" checked />
+          <div class="radio-tile">全体公開</div>
+        </label>
+        <label class="input-container">
+          <input class="radio-button" type="radio" name="open" />
+          <div class="radio-tile">フォロワー限定</div>
+        </label>
+      </div>
+    </form>
+    <form action="" method="post" class="order">
+      <div class="radio-tile-group">
+        <label class="input-container">
+          <input class="radio-button" type="radio" name="order" checked />
+          <div class="radio-tile">投稿順</div>
+        </label>
+        <label class="input-container">
+          <input class="radio-button" type="radio" name="order" />
+          <div class="radio-tile">人気順</div>
+        </label>
+        <label class="input-container">
+          <input class="radio-button" type="radio" name="order" />
+          <div class="radio-tile">閲覧数順</div>
+        </label>
+      </div>
+    </form>
+  </section>
+  <div class="pagination">
+    <ul>
+      <li class="list-item"><a href="">◀︎</a></li>
+      <li class="list-item"><a href="">1</a></li>
+      <li class="list-item"><a href="">2</a></li>
+      <li class="list-item"><a href="">3</a></li>
+      <li class="list-item"><a href="">4</a></li>
+      <li class="list-item"><a href="">5</a></li>
+      <li class="list-item"><a href="">6</a></li>
+      <li class="list-item"><a href="">7</a></li>
+      <li class="list-item"><a href="">▶︎</a></li>
+    </ul>
+  </div>
+  <div class="my-pic">
+    <div class="pic-container">
+      <div class="pic-flex">
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+        <div class="picture">
+          <img src="img/1438164.jpg">
+          <div class="pic-info">
+            <div class="pic-title"><a>タイトル</a></div>
+            <div class="first-col">
+              <a class="counter">ブックマーク: 8</a>
+              <a class="counter">コメント: 4件</a>
+            </div>
+            <div class="second-col">
+              <a class="counter">閲覧数: 3134</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="pagination">
+      <ul>
+        <li class="list-item"><a href="">◀︎</a></li>
+        <li class="list-item"><a href="">1</a></li>
+        <li class="list-item"><a href="">2</a></li>
+        <li class="list-item"><a href="">3</a></li>
+        <li class="list-item"><a href="">4</a></li>
+        <li class="list-item"><a href="">5</a></li>
+        <li class="list-item"><a href="">6</a></li>
+        <li class="list-item"><a href="">7</a></li>
+        <li class="list-item"><a href="">▶︎</a></li>
+      </ul>
+    </div>
+  </div>
+
+
+  <?php
+  require('footer.php');
+  ?>
