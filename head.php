f781d5221fdc3fd2c25cbdd400c9a2f65ba1e8f5
ikatyusya8 <48384384+ikatyusya8@users.noreply.github.com>
2019-12-21 15:59:54 +0900

    Create head.php

---
+++ head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title><?php echo $siteTitle; ?></title>
+    <meta charset="UTF-8">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  </head>
